9b633603816cf784f91353d91012f1b795c6792e
MarianLHG <161777122+MarianLHG@users.noreply.github.com>
2025-05-19 19:50:25 -0600

    Practica 1

---
+++ Practica 1/ejercicioC.js
@@ -0,0 +1,4 @@
+
+const saludoPersonalizado = (nombre, edad) => `Hola, soy ${nombre} y tengo ${edad} años.`
+
+console.log(saludoPersonalizado("Marian", 21));
---
+++ Practica 1/ejerciciob.js
@@ -0,0 +1,4 @@
+const cuadrado = (numero) => numero * numero;
+
+console.log(cuadrado(19));
+//muestra en consola probando con 3 numeros dif.
