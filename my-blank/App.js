b1816df46e5f44d516fa2f77e8c1b8f75a59a271
MarianLHG <161777122+MarianLHG@users.noreply.github.com>
2025-06-11 18:20:33 -0600

    objeto texto con propiedad contenido

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,15 +1,28 @@
+/* ZONA 1: Importaciones*/
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button} from 'react-native';
 
+const Texto=(props) => {
+  const {contenido}=props
+  return (
+    <Text>{contenido}</Text>
+  )
+}
+
+/* ZONA 2: Main */
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto contenido="hola"> </Texto>
+      <Texto contenido="mundo"> </Texto>
+      <Texto contenido="React Native"> </Texto>
+      <Button title="Presioname, pero no tanto"></Button>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+/* ZONA 3: Estilos */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
